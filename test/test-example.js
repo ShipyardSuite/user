a71e0ae6d230ee9eb702b8cf007b6d6d5cbb7c95
Frederik Reich <frederikreich@googlemail.com>
2020-04-01 17:26:38 +0200

    Add files

---
+++ test/test-example.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect;
+
+describe('Hello world', function() {
+	describe('boolean test operation', function() {
+		it('true should be true', function() {
+			expect(true).to.equal(true);
+		});
+	});
+});
